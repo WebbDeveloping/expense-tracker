2fc6123196a0134c0c00afee002215125b3974a9
Joe Webb <webbdeveloping@gmail.com>
2022-09-28 16:51:56 -0600

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 export default function ExpenseItem() {
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.76;
     return (
         <div>
-            <h2>Expensie item!</h2>
+            <div>March 28th 2022</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$297.64</div>
+            </div>
         </div>
 
     )
